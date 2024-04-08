0ede1d514339abb443298f9a85d59107080f3bbd
maxnm47 <maxnm47@gmail.com>
2024-04-08 13:27:27 +0200

    added maker

---
+++ antlrmaker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Run the antlr command
+java -jar /usr/local/lib/antlr-4.13.1-complete.jar -Dlanguage=CSharp -no-listener -visitor UCM.g4
+
+mkdir UCM/build
+
+# Move the generated .cs files to the UCM directory
+mv UCMParser.cs UCM/build
+mv UCMLexer.cs UCM/build
+
+
+# Move the generated .interp and .tokens files to the UCM directory, excluding the .g4 file
+mv UCM*.interp UCM/build
+mv UCM*.tokens UCM/build
+
+mv UCMBaseVisitor.cs UCM/typechecker
+mv UCMVisitor.cs UCM/typechecker
+
+
