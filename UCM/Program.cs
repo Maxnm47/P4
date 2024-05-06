c7f960f252c96c684fb50dc9abbcfc50360b3529
maxnm47 <maxnm47@gmail.com>
2024-05-06 11:27:04 +0200

    added test files

--- UCM/Program.cs
+++ UCM/Program.cs
@@ -1,4 +1,6 @@
-﻿using Antlr4.Runtime;
+﻿using System;
+using System.IO;
+using Antlr4.Runtime;
 using UCM;
 using UCM.ast;
 using UCM.astVisitor;
@@ -7,31 +9,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        string filePath = "balls.ucm";
+        //testing
+        string rootPath = args.Length > 0 ? args[0] : "UCM_TestFiles";
+        ExecuteUcmFiles(rootPath);
+
+        // string rootPath = "balls.ucm";
+        // ExecuteUcmFile(rootPath);
+    }
+
+    static void ExecuteUcmFiles(string directoryPath)
+    {
+        // Recursively handle all subdirectories
+        foreach (var subdirectory in Directory.GetDirectories(directoryPath))
+        {
+            ExecuteUcmFiles(subdirectory);
+        }
+
+        // Execute each .ucm file in the current directory
+        foreach (var filePath in Directory.GetFiles(directoryPath, "*.ucm"))
+        {
+            ExecuteUcmFile(filePath);
+        }
+    }
+
+    static void ExecuteUcmFile(string filePath)
+    {
+        Console.WriteLine("Executing file: " + filePath);
         string input = File.ReadAllText(filePath);
 
         ICharStream stream = CharStreams.fromString(input);
 
-        //create tokens
+        // Create tokens
         UCMLexer lexer = new UCMLexer(stream);
-
-        CommonTokenStream tokens = new CommonTokenStream(lexer); //makes tokens
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
 
         // Create parser
         UCMParser parser = new UCMParser(tokens);
+        UCMParser.RootContext parseTree = parser.root(); 
 
-        UCMParser.RootContext parseTree = parser.root(); //create tree
-
+        // Build AST
         AstBuildVisitor astBuildVisitor = new AstBuildVisitor();
         AstNode ast = astBuildVisitor.VisitRoot(parseTree);
 
-        Console.WriteLine(ast.ToString());
+        // Semantic Analysis
         SemanticAnalysisVisitor semanticAnalyser = new SemanticAnalysisVisitor();
         semanticAnalyser.Visit(ast);
 
-
-        // Console.WriteLine("AST done");
-        // Console.WriteLine(ast.ToString());
     }
 }
-
