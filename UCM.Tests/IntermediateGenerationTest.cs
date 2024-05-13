27c22b724ae02474e63faccbff95f2c467a8d0ec
maxnm47 <maxnm47@gmail.com>
2024-05-13 12:09:04 +0200

    added typechecker tests, intermediategenerationtests...

---
+++ UCM.Tests/IntermediateGenerationTest.cs
@@ -0,0 +1,185 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UCM.astVisitor;
+using UCM.ast;
+using UCM.astJunior;
+using Antlr4.Runtime;
+
+namespace UCM.Tests
+{
+    [TestClass]
+    public class IntermediateGenerationTests
+    {
+        private IntermediateGenerationVisitor visitor;
+        private AstBuildVisitor astBuilder;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            visitor = new IntermediateGenerationVisitor();
+            astBuilder = new AstBuildVisitor();  // Assuming you have this setup based on your AST nodes
+        }
+
+        private JAstNode GenerateIntermediateRepresentation(string code)
+        {
+            var stream = CharStreams.fromString(code);
+            var lexer = new UCMLexer(stream);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new UCMParser(tokens);
+            var parseTree = parser.root();
+            AstNode astRoot = astBuilder.VisitRoot(parseTree);
+            return visitor.VisitRoot(astRoot as RootNode);
+        }
+
+        [TestMethod]
+        public void TestSimpleVariableDeclaration()
+        {
+            string code = "int x = 5;";
+            JAstNode result = GenerateIntermediateRepresentation(code);
+            Assert.IsInstanceOfType(result, typeof(JObjectNode));
+            JObjectNode objectNode = result as JObjectNode;
+            Assert.AreEqual(1, objectNode.Fields.Count);
+            JFieldNode field = objectNode.Fields.First();
+            Assert.AreEqual("x", field.Key.Value);
+            Assert.IsInstanceOfType(field.Value, typeof(JIntNode));
+        }
+
+        [TestMethod]
+        public void TestComplexVariableDeclaration()
+        {
+            string code = @"
+                int x = 10;
+                object a = 
+                    { 
+                        int x = 10;
+                    };
+             ";
+            JAstNode result = GenerateIntermediateRepresentation(code);
+            Assert.IsInstanceOfType(result, typeof(JObjectNode));
+            JObjectNode rootNode = result as JObjectNode;
+            JFieldNode outerx = rootNode.Fields[0];
+            Assert.AreEqual("x", outerx.Key.Value); //outer x
+            Assert.IsInstanceOfType(outerx.Value, typeof(JIntNode));
+            Assert.AreEqual(2, rootNode.Fields.Count());
+
+            JFieldNode outerObject = rootNode.Fields[1];
+            Assert.AreEqual("a", outerObject.Key.Value);
+            Assert.IsInstanceOfType(outerObject.Value, typeof(JObjectNode));
+
+            JObjectNode innerObject = outerObject.Value as JObjectNode;
+            JFieldNode innerx = innerObject.Fields[0];
+            Assert.AreEqual("x", innerx.Key.Value); //inner x
+            Assert.IsInstanceOfType(innerx.Value, typeof(JIntNode));
+        }
+
+        [TestMethod]
+        public void testAssignment()
+        {
+            string code = @"
+                int x = 10;
+                int b = x;
+            ";
+            JAstNode result = GenerateIntermediateRepresentation(code);
+            Assert.IsInstanceOfType(result, typeof(JObjectNode));
+            JObjectNode rootNode = result as JObjectNode;
+            JFieldNode x = rootNode.Fields[0];
+            Assert.AreEqual("x", x.Key.Value);
+            Assert.IsInstanceOfType(x.Value, typeof(JIntNode));
+
+            JFieldNode b = rootNode.Fields[1];
+            Assert.AreEqual("b", b.Key.Value);
+            Assert.IsInstanceOfType(b.Value, typeof(JIntNode));
+        }
+
+        [TestMethod]
+        public void testAssigmentInScope()
+        {
+            string code = @"
+                int x = 10;
+                object a = 
+                {
+                    int x = x;
+                };
+            ";
+            JAstNode result = GenerateIntermediateRepresentation(code);
+            Assert.IsInstanceOfType(result, typeof(JObjectNode));
+            JObjectNode rootNode = result as JObjectNode;
+            JFieldNode x = rootNode.Fields[0];
+            Assert.AreEqual("x", x.Key.Value);
+            Assert.IsInstanceOfType(x.Value, typeof(JIntNode));
+
+            JObjectNode scope = rootNode.Fields[1].Value as JObjectNode;
+            JFieldNode innerx = scope.Fields[0];
+            Assert.AreEqual("x", innerx.Key.Value);
+            Assert.IsInstanceOfType(innerx.Value, typeof(JIntNode));
+
+            JIntNode innerxValue = innerx.Value as JIntNode;
+            Assert.AreEqual(10, innerxValue.Value);
+
+        }
+
+
+        [TestMethod]
+        public void testArray()
+        {
+            string code = @"
+                int[] x = [1,2,3];
+            ";
+            JAstNode result = GenerateIntermediateRepresentation(code);
+            Assert.IsInstanceOfType(result, typeof(JObjectNode));
+            JObjectNode rootNode = result as JObjectNode;
+            JFieldNode x = rootNode.Fields[0];
+            Assert.AreEqual("x", x.Key.Value);
+            Assert.IsInstanceOfType(x.Value, typeof(JArrayNode));
+
+            JArrayNode array = x.Value as JArrayNode;
+            Assert.AreEqual(3, array.Elements.Count);
+            Assert.IsInstanceOfType(array.Elements[0], typeof(JIntNode));
+            Assert.IsInstanceOfType(array.Elements[1], typeof(JIntNode));
+            Assert.IsInstanceOfType(array.Elements[2], typeof(JIntNode));
+        }
+
+        [TestMethod]
+        public void AssignToNonexisting()
+        {
+            string code = @"
+                int x = y;
+            ";
+            Assert.ThrowsException<System.Exception>(() => GenerateIntermediateRepresentation(code));
+        }
+
+        [TestMethod]
+        public void AssignToNonexistingInScope()
+        {
+            string code = @"
+                object a = 
+                {
+                    int x = y;
+                };
+            ";
+            Assert.ThrowsException<System.Exception>(() => GenerateIntermediateRepresentation(code));
+        }
+
+        [TestMethod]
+        public void AssignToNonexistingInArray()
+        {
+            string code = @"
+                int[] x = [1,y,3];
+            ";
+            Assert.ThrowsException<System.Exception>(() => GenerateIntermediateRepresentation(code));
+        }
+
+        [TestMethod]
+        public void AssignFutureScope()
+        {
+
+            string code = @"
+                int a = a.y;
+                object a = 
+                {
+                    int y = 10;   
+                };
+            ";
+            Assert.ThrowsException<System.Exception>(() => GenerateIntermediateRepresentation(code));
+        }
+    }
+}
---
+++ UCM.Tests/TypeCheckerTests.cs
@@ -0,0 +1,225 @@
+using System.Linq.Expressions;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Atn;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UCM;
+using UCM.ast;
+using UCM.ast.complexValues;
+using UCM.ast.loopConstruction;
+using UCM.ast.numExpr;
+using UCM.ast.root;
+using UCM.typechecker;
+namespace UCM.astVisitor;
+
+[TestClass]
+public class TypeCheckerTest
+{
+    private SemanticAnalysisVisitor typeChecker;
+    private AstBuildVisitor visitor; 
+    private UCMLexer lexer;
+    private UCMParser parser;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        typeChecker = new SemanticAnalysisVisitor();
+        visitor = new AstBuildVisitor(); 
+    }
+
+    private AstNode GetNode(string program)
+    {
+        var stream = CharStreams.fromString(program);
+        lexer = new UCMLexer(stream);
+        var tokens = new CommonTokenStream(lexer);
+        parser = new UCMParser(tokens);
+        var parseTree = parser.root();
+        return visitor.VisitRoot(parseTree); // Now visitor is initialized
+    }
+
+    private AstNode GetTypeCheckedAst(string program)
+    {
+        AstNode rootNode = GetNode(program);
+        return typeChecker.VisitRoot(rootNode as RootNode);
+    }
+
+    [TestMethod]
+    public void CorrectlyTypedInt()
+    {
+        string program = "int x = 5;";
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        IntNode intnode = root.Fields[0].Expr.GetChild<IntNode>(0);
+        Assert.AreEqual(5, intnode.value);
+        Assert.AreEqual(TypeEnum.Int, intnode.typeInfo.type);
+    }
+
+
+    [TestMethod]
+    public void CorrectlyTypedFloat()
+    {
+        string program = "float x = 5.0;";
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        FloatNode floatnode = root.Fields[0].Expr.GetChild<FloatNode>(0);
+        Assert.AreEqual(5.0, floatnode.value);
+        Assert.AreEqual(TypeEnum.Float, floatnode.typeInfo.type);
+    }
+
+    [TestMethod]
+    public void CorrectlyTypedString()
+    {
+        string program = "string x = \"Hello\";";
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        StringNode stringnode = root.Fields[0].Expr.GetChild<StringNode>(0);
+        Assert.AreEqual("Hello", stringnode.value);
+        Assert.AreEqual(TypeEnum.String, stringnode.typeInfo.type);
+    }
+
+    [TestMethod]
+    public void CorrectlyTypedBool()
+    {
+        string program = "bool x = true;";
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        BoolNode boolnode = root.Fields[0].Expr.GetChild<BoolNode>(0);
+        Assert.AreEqual(true, boolnode.value);
+        Assert.AreEqual(TypeEnum.Bool, boolnode.typeInfo.type);
+    }
+
+    [TestMethod]
+    public void TestCorrectTypes()
+    {
+        string program = "int x = 5;";
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        Assert.AreEqual(5, root.Fields[0].Expr.GetChild<IntNode>(0).value);
+    }
+    [TestMethod]
+    public void TestIncorrectTypes()
+    {
+        string program = "int x = 5.0;"; // This should lead to a type mismatch error
+        try
+        {
+            RootNode root = (RootNode)GetTypeCheckedAst(program);
+            var field =  root.Fields[0].Expr.GetChild<IntNode>(0);
+            Assert.AreEqual(5, field.value);
+
+
+        }
+        catch (Exception ex)
+        {
+            // EXTREMELY HACKY
+            Assert.AreEqual("Semantic analysis failed", ex.Message);
+        }
+    }
+
+    [TestMethod]
+    public void TestCorrectTypesInObject()
+    {
+        string program = "object a = {int x = 5; int y = 10;};"; 
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        var objectNode = root.Fields[0].Expr.GetChild<ObjectNode>(0);
+        Assert.AreEqual(5, objectNode.Fields[0].Expr.GetChild<IntNode>(0).value);
+        Assert.AreEqual(10, objectNode.Fields[1].Expr.GetChild<IntNode>(0).value);
+
+        Assert.AreEqual(TypeEnum.Int, objectNode.Fields[0].Expr.GetChild<IntNode>(0).typeInfo.type);
+        Assert.AreEqual(TypeEnum.Int, objectNode.Fields[1].Expr.GetChild<IntNode>(0).typeInfo.type);        
+    }
+
+    [TestMethod]
+    public void testIncorrectTypesInObject(){
+        string program = "object a = {int x = 5; int y = 10.0;};"; 
+        try
+        {
+            RootNode root = (RootNode)GetTypeCheckedAst(program);
+            var objectNode = root.Fields[0].Expr.GetChild<ObjectNode>(0);
+            Assert.AreEqual(5, objectNode.Fields[0].Expr.GetChild<IntNode>(0).value);
+            Assert.AreEqual(10, objectNode.Fields[1].Expr.GetChild<IntNode>(0).value);
+
+            Assert.AreEqual(TypeEnum.Int, objectNode.Fields[0].Expr.GetChild<IntNode>(0).typeInfo.type);
+            Assert.AreEqual(TypeEnum.Int, objectNode.Fields[1].Expr.GetChild<IntNode>(0).typeInfo.type);        
+        }
+        catch (Exception ex)
+        {
+            // EXTREMELY HACKY
+            Assert.AreEqual("Semantic analysis failed", ex.Message);
+        }
+    }
+
+    [TestMethod]
+    public void TestCorrectTypesInArray()
+    {
+        string program = "int[] a = [1,2,3];"; 
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        var arrayNode = root.Fields[0].Expr.GetChild<ArrayNode>(0);
+        Assert.AreEqual(1, arrayNode.Elements[0].GetChild<IntNode>(0).value);
+        Assert.AreEqual(2, arrayNode.Elements[1].GetChild<IntNode>(0).value);
+        Assert.AreEqual(3, arrayNode.Elements[2].GetChild<IntNode>(0).value);
+
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[0].typeInfo.type);
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[1].typeInfo.type);
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[2].typeInfo.type);
+    }
+
+    [TestMethod]
+    public void TestIncorrectArrayTypes(){
+        string program = "int[] a = [1,2.0,3];"; 
+        try
+        {
+            RootNode root = (RootNode)GetTypeCheckedAst(program);
+            var arrayNode = root.Fields[0].Expr.GetChild<ArrayNode>(0);
+            Assert.AreEqual(1, arrayNode.Elements[0].GetChild<IntNode>(0).value);
+            Assert.AreEqual(2, arrayNode.Elements[1].GetChild<IntNode>(0).value);
+            Assert.AreEqual(3, arrayNode.Elements[2].GetChild<IntNode>(0).value);
+
+            Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[0].typeInfo.type);
+            Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[1].typeInfo.type);
+            Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[2].typeInfo.type);
+        }
+        catch (Exception ex)
+        {
+            // EXTREMELY HACKY
+            Assert.AreEqual("Semantic analysis failed", ex.Message);
+        }
+    }
+
+    [TestMethod]
+    public void TestCorrectTypesInLoop()
+    {
+        string program = "object a = {for(int i in [1,2,3]){int x = i;}};"; 
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        var objectNode = root.Fields[0].Expr.GetChild<ObjectNode>(0);
+        var loopNode = objectNode.Loops[0];
+        var arrayNode = loopNode.Array.GetChild<ArrayNode>(0);
+        Assert.AreEqual(1, arrayNode.Elements[0].GetChild<IntNode>(0).value);
+        Assert.AreEqual(2, arrayNode.Elements[1].GetChild<IntNode>(0).value);
+        Assert.AreEqual(3, arrayNode.Elements[2].GetChild<IntNode>(0).value);
+
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[0].typeInfo.type);
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[1].typeInfo.type);
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[2].typeInfo.type);
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[0].GetChild<IntNode>(0).typeInfo.type);
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[1].GetChild<IntNode>(0).typeInfo.type);
+        Assert.AreEqual(TypeEnum.Int, arrayNode.Elements[2].GetChild<IntNode>(0).typeInfo.type);
+    }
+
+    [TestMethod]
+    public void TestTemplateObject(){
+        string program = """
+        template A{
+                int b;
+            }
+        A a = {
+            b = 10;
+        };
+        """;
+
+        RootNode root = (RootNode)GetTypeCheckedAst(program);
+        var fieldNode = root.Fields[0];
+        Assert.AreEqual("a", fieldNode.Key.Id.value);
+        Assert.AreEqual("A", fieldNode.Type.value);
+        var templateObject = fieldNode.Expr.GetChild<ObjectNode>(0);
+        var field = templateObject.Fields[0];
+        Assert.AreEqual("b", field.Key.Id.value);
+        Assert.AreEqual(10, field.Expr.GetChild<IntNode>(0).value);
+
+        Assert.AreEqual(TypeEnum.Int, field.Expr.GetChild<IntNode>(0).typeInfo.type);   
+    }
+
+}
