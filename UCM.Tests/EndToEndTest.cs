63fbb40180f78ebb5bbdc5464fb7a885dd495ce5
maxnm47 <maxnm47@gmail.com>
2024-05-14 12:08:54 +0200

    wip

--- UCM.Tests/EndToEndTest.cs
+++ UCM.Tests/EndToEndTest.cs
@@ -11,6 +11,7 @@ using UCM.astJunior;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using UCM.UCMJuniorGeneration;
+using UCM.typechecker;
 
 namespace UCM.Tests;
 
@@ -20,6 +21,7 @@ public class EndToEndTest
     [TestMethod]
     public void SmallEndtoEndTest()
     {
+        //this test is to ensure the value is 10 and the type is int throughout the entire process.
         string input = @"
             int a = 10;
         ";
@@ -32,17 +34,41 @@ public class EndToEndTest
         lexer.RemoveErrorListeners();
         lexer.AddErrorListener(new ErrorListener());
 
-
         // Create parser 
         UCMParser parser = new UCMParser(tokens);
         UCMParser.RootContext parseTree = parser.root();
         //add error listenr
         parser.RemoveErrorListeners();
         parser.AddErrorListener(new ErrorListener());
+        //ensure each parser rule is correct
+        //ensure a is the id
+        UCMParser.FieldContext parserField = parseTree.field(0);
+        UCMParser.FieldIdContext fieldId = parserField.fieldId();
+        UCMParser.IdContext id = fieldId.id();
+        Assert.AreEqual("a", id.GetText());
+
+        //ensure the type is int
+        UCMParser.TypeContext type = parserField.type();
+        UCMParser.PrimitiveTypeContext primitiveType = type.primitiveType();
+        Assert.IsNotNull(primitiveType.INT_T());
+        Assert.IsNull(primitiveType.FLOAT_T());
+
+        //ensure value is 10.
+        UCMParser.ExprContext expr = parserField.expr();
+        UCMParser.ValueContext value = expr.value();
+        UCMParser.NumContext num = value.num();
+        Assert.AreEqual("10", num.GetText());
+                
 
         // Build AST TEST AST
         AstBuildVisitor astBuildVisitor = new AstBuildVisitor();
         AstNode ast = astBuildVisitor.VisitRoot(parseTree);
+        var rootNode = (RootNode)astBuildVisitor.VisitRoot(parseTree);
+        var fieldNode = rootNode.Fields[0];
+        //check the fields
+        Assert.AreEqual("a", fieldNode.Key.Id.value);
+        Assert.AreEqual(TypeEnum.Int, ((TypeAnotationNode)fieldNode.Type).type); 
+        Assert.AreEqual(10, fieldNode.Expr.GetChild<IntNode>(0).value);
 
         // Semantic Analysis TEST SEMANTIC ANALYSIS
         SemanticAnalysisVisitor semanticAnalyser = new SemanticAnalysisVisitor();
